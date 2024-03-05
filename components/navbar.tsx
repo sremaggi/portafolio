9976be8adf0363428adb086ab490a8e46250ec6f
Sebastián Remaggi Flores <ext_sebasremaggi@falabella.cl>
2024-03-05 20:37:01 -0300

    I fix the navbar error, when i touch it, this not navigate to the other pages

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,21 +6,37 @@ import { itemsNavbar } from "@/data";
 
 import { MotionTransition } from "./transition-component";
 import { usePathname } from "next/navigation";
+import React from "react";
+
+interface NavbarItemProps {
+    item: {
+      id: number; // Actualizado para aceptar un número
+      link: string;
+      icon: JSX.Element;
+    };
+    router: string;
+  }
+  
+  const NavbarItem: React.FC<NavbarItemProps> = ({ item, router }) => (
+    
+    <Link href={item.link}
+      key={item.id.toString()} // Convierte el id a string para usarlo como key
+      className={`px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary ${router === item.link && 'bg-secondary'}`}
+      data-tooltip-target="tooltip-default">
+     {item.icon}
+    </Link>
+  );
+  
 
 const Navbar = () => {
-    const router = usePathname()
+    const router = usePathname();
 
     return (
         <MotionTransition position="right" className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto h-max bottom-2">
             <nav>
                 <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 background-blur-sm">
                     {itemsNavbar.map((item) => (
-                        <div
-                            key={item.id}
-                            className={`px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary ${router === item.link && 'bg-secondary'}`}
-                            data-tooltip-target="tooltip-default">
-                            <Link href={item.link}>{item.icon} </Link>
-                        </div>
+                        <NavbarItem key={item.id} item={item} router={router} />
                     ))}
                 </div>
             </nav>
